952f1ec53cd02e6437fd70363b228a219174ee92
kwak-bs <iam-bs@naver.com>
2021-07-30 18:27:16 +0900

    Style #1  : Implement initial UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import RecentList from './Pages/RecentList';
-
+import ProductList from './Pages/ProductList';
+import DetailedProduct from 'Pages/DetailedProduct';
 function App() {
   return (
     <div className="App">
       {/* <h1>화이팅!</h1> */}
-      <RecentList/>
+      <DetailedProduct/>
     </div>
   );
 }
